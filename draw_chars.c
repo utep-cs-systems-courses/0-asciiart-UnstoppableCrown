e8c87b910ed7132dcb796ca9f2a8f1aec63f0ddc
Jacob <jagonzalez63@miners.utep.edu>
2023-02-08 10:46:19 -0700

    Final

--- draw_chars.c
+++ draw_chars.c
@@ -14,13 +14,13 @@ void print_char_11x16(char c)
   }
 }
 
-void print_char_8x12(char c)
+void print_char_5x7(char c)
 {
   c -= 0x20;
-  for (char row = 0; row < 10; row++) {
-    unsigned short rowBits = font_8x12[c][row];
-    for(char col = 0; col < 12; col++) {
-      unsigned short colMask = 1 << (11-col);
+  for (char row = 0; row < 5; row++) {
+    unsigned short rowBits = font_5x7[c][row];
+    for(char col = 0; col < 7; col++) {
+      unsigned short colMask = 1 << (6-col);
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
     putchar('\n');
